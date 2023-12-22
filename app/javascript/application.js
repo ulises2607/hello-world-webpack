2c90dbe82dfb3da8ba47772cba6c40ef73bc160d
Ulises <ulisescappa@gmail.com>
2023-12-22 16:40:44 -0300

    Setup rails-react app with webpack.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,8 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
-  return (<h1>Hello World!</h1>);
+  return (<h1>Hello 2!</h1>);
 }
 
 ReactDOM.render(
